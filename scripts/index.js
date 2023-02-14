a7d7449fa85792e52765314ae36c817bf40b7db3
Sergey <sergey14fedorov@gmail.com>
2023-02-14 19:22:11 +0300

    fix second

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,10 @@
 // console.log('hello');
-const openPopup = document.querySelector('.profile__popup-open');
+const openPopup = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__button-close');
 
 openPopup.addEventListener('click', toggleClass)//пишем функцию без скобок, чтобы она сразу не вызвалась
-
-
-closePopup.addEventListener('click', () => popup.classList.toggle('popup_opened'))//можно сделать при помощи стрелочной функции в качестве кол-бека. 
+closePopup.addEventListener('click', toggleClass)
 
 function toggleClass() {//параметр event передается с addEventListener. 
   popup.classList.toggle('popup_opened');// .toggle() позволяет отобразить или скрыть выбранные элементы. 
@@ -31,46 +29,29 @@ let formElement = document.querySelector('.popup__form')
 let nameInput = formElement.querySelector('.popup__name')
 let jobInput = formElement.querySelector('.popup__job')
 
-
-const userName = 'Жак-Ив Кусто'
-const userJob = 'Исследователь океана'
-
-
+// находим элементы с именем и работой пользователя
 const userNameElement = document.querySelector('.profile__title')
-userNameElement.textContent = userName;
-
 const userJobElement = document.querySelector('.profile__subtitle')
-userJobElement.textContent = userJob;
-
+// находим инпуты имени и работы в форме
 const userNameInput = document.querySelector('.popup__name')
-userNameInput.value = userName;
-
 const userJobInput = document.querySelector('.popup__job')
-userJobInput.value = userJob;
+// записываем в эти поля значения с текущих значений юзера
+userNameInput.value = userNameElement.textContent;
+userJobInput.value = userJobElement.textContent;
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Получите значение полей jobInput и nameInput из свойства value
-
-  // Выберите элементы, куда должны быть вставлены значения полей
-  // const userNameElement = document.querySelector('.profile__title')
   userNameElement.textContent = nameInput.value;
-
-  // const userJobElement = document.querySelector('.profile__subtitle')
   userJobElement.textContent = jobInput.value;
-
-  // Вставьте новые значения с помощью textContent
+  toggleClass();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
-formElement.addEventListener('submit', toggleClass);
 
 
 
