914b0f78e0cf4907b3d807d5661719fa4f286030
Sergey <sergey14fedorov@gmail.com>
2023-02-18 16:22:39 +0300

    second fix

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,13 @@ const openPopup = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__button-close');
 
-openPopup.addEventListener('click', toggleClass)//пишем функцию без скобок, чтобы она сразу не вызвалась
-closePopup.addEventListener('click', toggleClass)
 
 function toggleClass() {//параметр event передается с addEventListener. 
+  userNameInput.value = userNameElement.textContent;
+  userJobInput.value = userJobElement.textContent;
   popup.classList.toggle('popup_opened');// .toggle() позволяет отобразить или скрыть выбранные элементы. 
   // Если элемент изначально отображается, то он будет скрыт, если элемент скрыт, то он будет отображен.
 }
-
-
-//  ЗАКРЫТИЕ ПОПАПА ПРИ НАЖАТИИ НА ОБЛАСТЬ ВНЕ ПОПАПА
-popup.addEventListener('click', function (event) {
-  console.log('target :', event.target);//сюда попадает тот элемент, по которому кликнули
-  console.log('currentTarget :', event.currentTarget);//тот элемент, на который мы повесили addEventListener. То есть класс .popup, который лежит в переменной popup
-  if (event.target === event.currentTarget) {
-    toggleClass();
-  }
-})
-
-
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form')
@@ -36,8 +24,6 @@ const userJobElement = document.querySelector('.profile__subtitle')
 const userNameInput = document.querySelector('.popup__input_type_name')
 const userJobInput = document.querySelector('.popup__input_type_job')
 // записываем в эти поля значения с текущих значений юзера
-userNameInput.value = userNameElement.textContent;
-userJobInput.value = userJobElement.textContent;
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -51,7 +37,16 @@ function handleFormSubmit(evt) {
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);//слушатель сохранения изменений в форме
+openPopup.addEventListener('click', toggleClass)//пишем функцию без скобок, чтобы она сразу не вызвалась
+closePopup.addEventListener('click', toggleClass)//слушатель закрытия попапа
+
+//  ЗАКРЫТИЕ ПОПАПА ПРИ НАЖАТИИ НА ОБЛАСТЬ ВНЕ ПОПАПА
+popup.addEventListener('click', function (event) {
+  if (event.target === event.currentTarget) {
+    toggleClass();
+  }
+})
 
 
 
