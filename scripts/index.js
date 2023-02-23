80d39dcfbddc4e10a11e58a43a9aa8f3654f7ba3
Sergey <sergey14fedorov@gmail.com>
2023-02-23 23:59:15 +0300

    feat: Рендеринг массива объектов

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,30 @@ const popupProfile = document.querySelector('.popup_profile');
 const popupProfileOpen = document.querySelector('.profile__edit-button');
 const popupProfileClose = popupProfile.querySelector('.popup__button-close');
 
-
-// Находим форму в DOM
-const formProfile = popupProfile.querySelector('.popup__form')//форму в переменную для обработки сабмита
-// находим элементы с именем и работой пользователя
 const userNameElement = document.querySelector('.profile__title')//на странице
 const userJobElement = document.querySelector('.profile__subtitle')
 
-
+const formProfile = popupProfile.querySelector('.popup__form')//форму в переменную для обработки сабмита
 const userNameInput = popupProfile.querySelector('.popup__input_type_name')//поля в форме
 const userJobInput = popupProfile.querySelector('.popup__input_type_job')
+
+
+//ПОПАП ДОБАВЛЕНИЯ НОВОЙ КАРТОЧКИ
+const popupNewCard = document.querySelector('.popup_new_card');
+const popupNewCardOpen = document.querySelector('.profile__add-button');
+const popupNewCardClose = popupNewCard.querySelector('.popup__button-close');
+
+const cardName = document.querySelector('.elements__title')//на странице
+const cardImage = document.querySelector('.elements__image')
+
+const formNewCard = popupNewCard.querySelector('.popup__form')//форму в переменную для обработки сабмита
+const formCardName = popupNewCard.querySelector('.popup__input_type_name')//поля в форме
+const formCardImage = popupNewCard.querySelector('.popup__input_type_image')
 
 //функция переключения класса видимости для всех попапов
 const togglePopup = (popup) => {
   popup.classList.toggle('popup_opened');
 }
-
 
 const handleFormSubmit = (evt) => {//отправка формы профиля evt -- параметр, передаваемый кликом
   evt.preventDefault(); // Отмена дефолтной отправки формы с перезагрузкой страницы
@@ -26,21 +34,24 @@ const handleFormSubmit = (evt) => {//отправка формы профиля 
   togglePopup(popupProfile);
 }
 
-popupProfile.addEventListener('click', (event) => {//  ЗАКРЫТИЕ ПОПАПА ПРИ НАЖАТИИ НА ОБЛАСТЬ ВНЕ ПОПАПА
-  if (event.target === event.currentTarget) {
-  togglePopup(popupProfile);
-  }
-})
+
 popupProfileOpen.addEventListener('click', () => {//открытие попапа профиля
   userNameInput.value = userNameElement.textContent;
   userJobInput.value = userJobElement.textContent;
   togglePopup(popupProfile);
 })
+
+formProfile.addEventListener('submit', handleFormSubmit);// сохранения изменений в форме профиля
+
 popupProfileClose.addEventListener('click', () => {//закрытие попапа профиля
   togglePopup(popupProfile)
 })
 
-formProfile.addEventListener('submit', handleFormSubmit);//слушатель сохранения изменений в форме профиля
+popupProfile.addEventListener('click', (event) => {//  ЗАКРЫТИЕ ПОПАПА ПРИ НАЖАТИИ НА ОБЛАСТЬ ВНЕ ПОПАПА
+  if (event.target === event.currentTarget) {
+  togglePopup(popupProfile);
+  }
+})
 
 
 
@@ -50,34 +61,51 @@ formProfile.addEventListener('submit', handleFormSubmit);//слушатель с
 const initialCards = [
   {
     name: 'Архыз',
-    image: 'https://picsum.photos/00?card1'
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
     name: 'Челябинская область',
-    image: 'https://picsum.photos/00?card2'
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
     name: 'Иваново',
-    image: 'https://picsum.photos/00?card3'
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
     name: 'Камчатка',
-    image: 'https://picsum.photos/00?card4'
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
     name: 'Холмогорский район',
-    image: 'https://picsum.photos/00?card5'
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
     name: 'Байкал',
-    image: 'https://picsum.photos/00?card6'
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
-// const elements = document.querySelector('.elements');
+const elements = document.querySelector('.elements');
 
-// const createCard = (card) => {
-//   const cardTemplate = document.querySelector('.template').value.cloneNode(true);
-//   const cardTitle = card.querySelector('.card__title');
-// }
+const createCard = (card) => {
+  const newCard = document.querySelector('.template').content.cloneNode(true);
+  const cardTitleTemplate = newCard.querySelector('.card__title');
+  cardTitleTemplate.textContent = card.name
+  const cardImageTemplate = newCard.querySelector('.card__image')
+  cardImageTemplate.setAttribute('src', card.image)
+  cardImageTemplate.setAttribute('alt', card.name)
+  const likeButton = newCard.querySelector('.card__trash-button')
+  likeButton.addEventListener('click', handleDeleteButtonClick)
+  elements.append(newCard)
+}
+
+initialCards.forEach(createCard);
+
+
+
+function handleDeleteButtonClick(event) {//удаление карточки
+  const button = event.target;
+  const card = button.closest('.card')//возможна проблема из-за того, что .card -- div, а не form
+  card.remove()
+}
 
